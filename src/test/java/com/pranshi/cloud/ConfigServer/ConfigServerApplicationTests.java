80c255c15a7d9cbf6b9cfcda80f7f69efcda3c66
Vipin <=vipinvats42@gmail.com>
2025-05-31 18:07:48 +0530

    reading the confriguration from class path

---
+++ src/test/java/com/pranshi/cloud/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranshi.cloud.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
